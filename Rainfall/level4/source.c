a4ff54c2b127904da6f2e279bb3edb5d1ff867fa
Xavier PERRIN <xperrin@student.42.fr>
2022-11-16 20:32:59 +0100

    Fixed source.c in level4

--- Rainfall/level4/source.c
+++ Rainfall/level4/source.c
@@ -3,12 +3,12 @@
 
 unsigned int m;
 
-void sym.n(void)
+void n(void)
 {
 	uint s;
 
-	sym.imp.fgets(&s, 0x200, stdin);
-	sym.p(&s);
+	fgets(&s, 0x200, stdin);
+	p(&s);
 	if (m == 0x1025544) {
 		system("/bin/cat /home/user/level5/.pass");
 	}
