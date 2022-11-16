0a0489d9f9ffe117770f40161107223e0aaa89ca
Xavier PERRIN <xperrin@student.42.fr>
2022-11-16 20:21:50 +0100

    level 4 source

---
+++ Rainfall/level4/source.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned int m;
+
+void sym.n(void)
+{
+	uint s;
+
+	sym.imp.fgets(&s, 0x200, stdin);
+	sym.p(&s);
+	if (m == 0x1025544) {
+		system("/bin/cat /home/user/level5/.pass");
+	}
+	return;
+}
+
+void main(void)
+{
+	n();
+	return;
+}
