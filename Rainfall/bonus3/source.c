61f0cd53808325790687fc025877ee0513c52ccf
pde-bakk <pde-bakk@student.codam.nl>
2022-11-23 18:37:22 +0100

    renamed v5 to file_ptr

--- Rainfall/bonus3/source.c
+++ Rainfall/bonus3/source.c
@@ -5,17 +5,17 @@
 int	main(int argc, const char **argv)
 {
 	char ptr[132];
-	FILE *v5;
+	FILE *file_ptr;
 
-	v5 = fopen("/home/user/end/.pass", "r");
+	file_ptr = fopen("/home/user/end/.pass", "r");
 	memset(ptr, 0, sizeof(ptr));
-	if (!v5 || argc != 2)
+	if (!file_ptr || argc != 2)
 		return -1;
-	fread(ptr, 1u, 66, v5);
+	fread(ptr, 1, 66, file_ptr);
 	ptr[65] = 0;
 	ptr[atoi(argv[1])] = 0;
-	fread(&ptr[66], 1u, 65, v5);
-	fclose(v5);
+	fread(&ptr[66], 1, 65, file_ptr);
+	fclose(file_ptr);
 	if (strcmp(ptr, argv[1]) == 0)
 		execl("/bin/sh", "sh", 0);
 	else
