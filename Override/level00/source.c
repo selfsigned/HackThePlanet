ac855919ff2311539f14010f70adca77618fabf1
pde-bakk <pde-bakk@student.codam.nl>
2022-11-23 23:16:55 +0100

    level 00: updated source file

--- Override/level00/source.c
+++ Override/level00/source.c
@@ -1,19 +1,24 @@
-bool main(void)
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
 {
-    int32_t aiStack20 [4];
+	int v4;
 
-    sym.imp.puts("***********************************");
-    sym.imp.puts("* \t     -Level00 -\t\t  *");
-    sym.imp.puts("***********************************");
-    sym.imp.printf("Password:");
-    sym.imp.__isoc99_scanf(0x8048636, aiStack20);
-    if (aiStack20[0] != 0x149c) {
-        sym.imp.puts("\nInvalid Password!");
-    }
-    else {
-        sym.imp.puts("\nAuthenticated!");
-        sym.imp.system("/bin/sh");
-    }
-    return aiStack20[0] != 0x149c;
+	puts("***********************************");
+	puts("* \t     -Level00 -\t\t  *");
+	puts("***********************************");
+	printf("Password:");
+	scanf("%d", &v4);
+	if (v4 == 5276)
+	{
+		puts("\nAuthenticated!");
+		system("/bin/sh");
+		return (0);
+	}
+	else
+	{
+		puts("\nInvalid Password!");
+		return (1);
+	}
 }
