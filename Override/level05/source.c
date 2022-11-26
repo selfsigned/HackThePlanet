0dae1a48176c4cc65a7eefeed466f64c0e7828f1
Xavier PERRIN <xperrin@student.42.fr>
2022-11-26 20:31:11 +0100

    WIP source file

---
+++ Override/level05/source.c
@@ -0,0 +1,15 @@
+int main()
+{
+  char s[100]; // [esp+28h] [ebp-70h] BYREF
+  unsigned int i; // [esp+8Ch] [ebp-Ch]
+
+  i = 0;
+  fgets(s, 100, stdin);
+  for ( i = 0; i < strlen(s); ++i )
+  {
+    if ( s[i] > 64 && s[i] <= 90 )
+      s[i] ^= 0x20u;
+  }
+  printf(s);
+  exit(0);
+}
